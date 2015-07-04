d2c062b2bf0c76214c2f91ad167d8d9bd0cecc55
Hector.Lee <lipeihydrogeology@gmail.com>
2015-07-05 01:28:27 +0300

    Finish the head file of virtual base class.

---
+++ header/ExplicitFiniteDifference.h
@@ -0,0 +1,13 @@
+#ifndef EXPLICITFINITEDIFFERENCE_H_
+#define EXPLICITFINITEDIFFERENCE_H_
+
+#include "BasicFiniteDifference.h"
+class ExplicitFiniteDifference : public BasicFiniteDifference 
+{
+private:
+	
+public:
+	
+}
+
+#endif
